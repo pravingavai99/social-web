a2d079068c33ae24437e3a0553d44cf64859f2d9
pravin <pravin.gavai99@gmail.com>
2019-12-14 18:49:29 +0530

    updated profile forms, importing size and models

--- users/forms.py
+++ users/forms.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from .models import Profile
 # Create your models here.
 
 class UserRegisterForm(UserCreationForm):
@@ -10,3 +11,16 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model= User
         fields= ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email= forms.EmailField()
+
+    class Meta:
+        model= User
+        fields= ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model= Profile
+        fields= ['image']
