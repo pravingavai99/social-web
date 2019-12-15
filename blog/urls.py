8501277cc239a7ac10217a9adb11f5b044fbd20d
pravin <pravin.gavai99@gmail.com>
2019-12-15 16:38:49 +0530

    CRUD added for posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from users import views as user_views
 
 urlpatterns = [
@@ -10,4 +10,7 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='about'),
+    #path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
+   # path('user/<str:username>/', UserPostListView.as_view(), name='user_post_list'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
@@ -12,16 +13,38 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'all_posts'
     ordering = ['-date_posted']
+    paginate_by = 8
 
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'all_posts'
+    paginate_by = 8
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+'''
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_post_list_list'
+    context_object_name = 'all_posts'
+    paginate_by = 8
+    def get_queryset(self):
+        user= get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+'''
 class PostDetailView(DetailView):
     model = Post
 
-class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title','content']
     def form_valid(self, form):
         form.instance.author= self.request.user #current user
         return super().form_valid(form)
+
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
@@ -46,4 +69,4 @@ class PostDeleteView(DeleteView, LoginRequiredMixin, UserPassesTestMixin):
         return False
 
 def about(request):
-    return render(request, 'blog/about.html')
+    return render(request, 'blog/about.html', {'title':'About'})
